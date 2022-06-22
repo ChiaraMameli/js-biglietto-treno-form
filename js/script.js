5c88b807df533a4a3146cc16bcc101021ca44a6e
ChiaraMameli <mameli.chiara@libero.it>
2022-06-22 15:52:35 +0200

    added input

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 console.log('JS OK')
 
+console.log('JS OK')
+
 // Prompt
 
-const kilometresTravel = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
-const ageTraveler = parseInt(prompt('Quanti anni hai?'));
-
-console.log(kilometresTravel);
-console.log(ageTraveler);
-
-// Calc
-
-let finalPrice = (kilometresTravel * 21) / 100;
-console.log(finalPrice);
-
-let finalPriceTeen = ''; 
-let finalPriceElderly = ''; 
+const kilometresInput = document.getElementById("kilometres");
+const ageInput = document.getElementById("age");
+const button = document.querySelector(".btn");
 
 const twentyDiscount = parseInt(20);
 const fortyDiscount = parseInt(40);
 
 const resultParagraph = document.getElementById('result');
 
-// Show
+button.addEventListener('click', function(){
+
+const kilometresTravel = kilometresInput.value;
+const ageTraveler = ageInput.value;
+
+console.log(kilometresTravel);
+console.log(ageTraveler);
+
+let finalPrice = kilometresTravel * 0.21;
+let finalPriceTeen = finalPrice * 0.8; 
+let finalPriceElderly = finalPrice * 0.6; 
 
 if (isNaN(kilometresTravel) || isNaN(ageTraveler)) {
     alert ('Il carattere inserito non è valido.')
@@ -29,18 +31,16 @@ if (isNaN(kilometresTravel) || isNaN(ageTraveler)) {
 } else {
 
 if (ageTraveler <= 18) {
-    finalPriceTeen = finalPrice - ((twentyDiscount / 100) * finalPrice);
-
     resultParagraph.innerHTML = `Hai ricevuto il 20% di sconto! Il prezzo del biglietto non è più <del> ${finalPrice}€ </del>, pagherai soltanto ${finalPriceTeen}€.`;
 
-} else if (ageTraveler >= 65) {
-    finalPriceElderly = finalPrice - ((fortyDiscount / 100) * finalPrice);
-    
+} else if (ageTraveler >= 65) {    
     resultParagraph.innerHTML = `Hai ricevuto il 40% di sconto! Il prezzo del biglietto non è più <del> ${finalPrice}€ </del>, pagherai soltanto ${finalPriceElderly}€.`;
-    
+   
 } else {
     resultParagraph.innerText = `Il prezzo del biglietto è ${finalPrice}€.`;
- 
+
 }
 
 }
+
+})
