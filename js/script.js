5fe5b8eb8a3b14ebe482ffb6690d9e31149e3021
ChiaraMameli <mameli.chiara@libero.it>
2022-06-23 16:22:14 +0200

    fixed error

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // Variabili
 const kilometresInput = document.getElementById("kilometres");
 const ageInput = document.getElementById("age");
-const button = document.querySelector(".btn");
+const sendButton = document.getElementById("send");
+const cancelButton = document.getElementById("cancel");
+
 const anagraphicsInput = document.getElementById("anagraphics");
 
 const twentyDiscount = parseInt(20);
@@ -9,17 +11,15 @@ const fortyDiscount = parseInt(40);
 
 const resultParagraph = document.getElementById('result');
 
-// Click
-
-button.addEventListener('click', function(){
-
 const kilometresTravel = parseInt(kilometresInput.value);
 const ageTraveler = parseInt(ageInput.value);
 const anagraphics = anagraphicsInput.value;
 
-console.log(kilometresTravel);
-console.log(ageTraveler);
+const ticketSection = document.getElementById('ticket-section');
 
+// Click
+
+sendButton.addEventListener('click', function(){
 let finalPrice = kilometresTravel * 0.21;
 
 if (!isNaN(kilometresInput) || !isNaN(ageInput)) {
@@ -34,17 +34,20 @@ if (ageTraveler >= 65) {
     finalPrice = finalPrice * 0.6; 
 }
 }
+
 const anagraphicsResult = document.getElementById('anagraphics-result');
-anagraphicsResult.innerText += ' ' + anagraphics;
+anagraphicsResult.innerHTML = `<h3 class="fs-6">NOME PASSEGGERO: </h2>${anagraphics}`;
 
 const kilometresResult = document.getElementById('kilometres-result')
-kilometresResult.innerText += ' ' + kilometresTravel;
+kilometresResult.innerHTML = `<h3 class="fs-6">CHILOMETRI: </h2>${kilometresTravel}`;
 
 const priceResult = document.getElementById('price-result')
-priceResult.innerText += ' ' + finalPrice;
-
-const ticketSection = document.getElementById('ticket-section');
+priceResult.innerHTML = `<h3 class="fs-6">TARIFFA: </h2>${finalPrice}`;
 
 ticketSection.className = '';
+})
 
+cancelButton.addEventListener('click', function() {
+    ticketSection.className = 'd-none';
+ 
 })
